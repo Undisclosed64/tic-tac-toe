8126f772fb27476eddb34c0152c249cd9f8c6b4d
Undisclosed <undisclosed674@gmail.com>
2021-06-12 21:10:01 -0700

    add function to check the win

--- script.js
+++ script.js
@@ -1,93 +1,140 @@
 
- //factory for creating player
- const players = function(name,symbol,turn){
-    return {name,symbol,turn}
-    };
-   
-  //Module
+//factory function for creating player 
+const players = function(name,symbol,turn){
+    return  {name,symbol,turn}
+}
+
+const player1 = players('Player1','X',true);
+const player2 = players('Player2','0',false);
+
+  //Module 
   const boardModule = (function(){
   
+//define necesarry variables
+let activePlayer;
+let gameState = true;
+
     //Array for game board
     let gameBoardArr= []; 
   
-    //function for letting user mark the boxes
+    //function for letting user play
     const play = function(){
     const buttons = document.querySelectorAll('button');
     buttons.forEach(function(btn){
     btn. addEventListener("click",function(e){
-  
-    //logic for letting player 1 mark the box one at a time
+
+   if(gameState === true){
+    //let player 1 mark the box 
      if(player1.turn==true && player2.turn==false && btn.textContent==""){
+       activePlayer = player1;
      btn.textContent = player1.symbol;
         player1.turn = false;
         player2.turn = true;
-     //   gameBoardArr.push(player1.symbol);
-   
-       //letting player 2 mark the boxes 
+        //console.log(activePlayer);
+
+
+       //let player 2 mark the box
     } else if(player1.turn==false &&
        player2.turn==true && btn.textContent == ""){
+     activePlayer = player2;
      btn.textContent = player2.symbol;
      player2.turn = false;
      player1.turn = true;
-   //gameBoardArr.push(player2.symbol);
-    }
+   //console.log(activePlayer);
+} 
+   };
+//check whether any player wins
+const check_win = checkWin();
   })
   })
     };
   
+  const checkWin = function(){
+    //select the boxes by their ordered ids and grab their textcontent
+    const box0 = document.getElementById('0').textContent;
+    const box1 = document.getElementById('1').textContent;
+    const box2 = document.getElementById('2').textContent;
+    const box3 = document.getElementById('3').textContent;
+    const box4 = document.getElementById('4').textContent;
+    const box5 = document.getElementById('5').textContent;
+    const box6 = document.getElementById('6').textContent;
+    const box7 = document.getElementById('7').textContent;
+    const box8 = document.getElementById('8').textContent;
   
-    let winningConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-  ];
-  
-  
-  const getId = function(){
-    const arr = [];
-    const elements = document.querySelectorAll('.btn');
-    elements.forEach(function(el){
-    el.addEventListener("click",function(e){
-  const clickedId = e.target.id;
-  //console.log(clickedId);
-  arr.push(parseInt(clickedId));
-  //console.log(arr);
-  while(arr.length>=3){
-   const splitArr = arr.splice(0,3);
-   gameBoardArr.push(splitArr);
-    //console.log(splitArr);
-   // console.log(gameBoardArr);
-   //console.log(arr);
-  // console.log(winningConditions);
-  
-  
-   }
-  //}
-  //}
-    })
-  })
+    checkRows();
+
+    //function for checking rows 
+    function checkRows(){
+      //check first row
+  if(box0 == activePlayer.symbol &&  box1 == activePlayer.symbol && box2 == activePlayer.symbol){
+      console.log(`${activePlayer.name} wins`);
+      gameState = false;
+  } 
+  //check second row
+  if(box3 == activePlayer.symbol &&  box4 == activePlayer.symbol && box5 == activePlayer.symbol){
+    console.log(`${activePlayer.name} wins`);
+    gameState = false;
   }
-    //returning object to be able to access outside the module
+  //check third row
+  if(box6 == activePlayer.symbol &&  box7 == activePlayer.symbol && box8 == activePlayer.symbol){
+    console.log(`${activePlayer.name} wins`);
+    gameState = false;
+  }
+}
+
+  checkCols();
+  function checkCols(){
+ //check first column
+ if(box0 == activePlayer.symbol && box3 == activePlayer.symbol && box6 == activePlayer.symbol){
+   console.log(`${activePlayer.name} wins`);
+   gameState = false;
+
+ }
+ //check second column
+ if(box1 == activePlayer.symbol && box4 == activePlayer.symbol && box7 == activePlayer.symbol){
+  console.log(`${activePlayer.name} wins`);
+  gameState = false;
+
+}
+//check third column 
+if(box2 == activePlayer.symbol && box5 == activePlayer.symbol && box8 == activePlayer.symbol){
+  console.log(`${activePlayer.name} wins`);
+  gameState = false;
+}
+  }
+
+ checkDiagnols();
+ function checkDiagnols(){
+//check diagnols
+if(box0 == activePlayer.symbol && box4 == activePlayer.symbol && box8 == activePlayer.symbol){
+  console.log(`${activePlayer.name} wins`);
+  gameState == false;
+}
+if(box2 == activePlayer.symbol && box4 == activePlayer.symbol && box6 == activePlayer.symbol){
+  console.log(`${activePlayer.name} wins`);
+  gameState == false;
+}
+  }
+}
+    //return object to be able to access outside the module
     return{
-      play,getId
+      play,
     }
-  
   })();
+  
   boardModule.play();
-  boardModule.winningConditions;
-  boardModule.getId();
-  
-  
-  const player1 = players('Player1','X',true);
-  const player2 = players('Player2','0',false);
-  
-  
-  
+
+  /*let winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+  */
   
   
   
